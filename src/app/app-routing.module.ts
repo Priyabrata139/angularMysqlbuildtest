6e0ff4e556c652d4d048b65d4514cd63e3c671b1
Priyabrata Maji <priyabratamaji139@gmail.com>
2023-03-09 16:37:24 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllUsersComponent } from './all-users/all-users.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"",component:HomeComponent},
+  { path:"users",component:AllUsersComponent },
+  { path:"user", component:NewUserComponent },
+  {path:"users/:id",component:NewUserComponent}
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
